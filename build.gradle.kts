0e6fd25767594c1e1e9a519dee01d7d349e91c02
Bekzat-Chauvbayev <kazna51k@gmail.com>
2023-12-09 16:04:00 +0600

    tracker app updated and view model also

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,15 +18,12 @@
 
 buildscript {
     extra.apply {
-        set("nav_version", "2.6.0")
+        set("lifecycle_version", "2.6.1")
         set("room_version", "2.5.2")
-        set("arch_lifecycle_version", "2.6.1")
     }
 }
-
 plugins {
     id("com.android.application") version "8.1.0" apply false
     id("com.android.library") version "8.1.0" apply false
     id("org.jetbrains.kotlin.android") version "1.9.0" apply false
-    id("androidx.navigation.safeargs") version "2.6.0" apply false
 }
